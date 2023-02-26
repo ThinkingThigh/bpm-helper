883073aa3232b5f87e4b2b458ebea602004f1519
liuyi03 <liuyi03@tal.com>
2023-02-26 21:34:32 +0800

    feat: 一些配置和尝试

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,23 @@ export default defineConfig(({ command, mode }) => {
   console.log("command&mode", command, mode);
   return {
     build: {
-      lib: {
-        entry: "./lib/main.js",
-        name: "main",
-        fileName: "main",
-      },
+      // lib: {
+      //   // entry: "./lib/main.js",
+      //   entry: "./lib/utils/bpmHelper.js",
+      //   // entry: "./public/js/index.js",
+      //   name: "index",
+      //   fileName: "index",
+      // },
+      assetsDir: "js",
+      // rollupOptions: {},
     },
     server: {
       open: "index.html",
       port: 80, // sudo lsof -i tcp:80 查看占用 PID, kill -9 PID 强制终止
       strictPort: true, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口。
+      proxy: {
+        // https://github.com/http-party/node-http-proxy#options
+      },
     },
   };
 });
