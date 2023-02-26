1c7aec5542e427eca1f6e1d063f3a58f54dc1cf9
liuyi03 <liuyi03@tal.com>
2023-02-26 16:41:10 +0800

    feat: 一些配置修改

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   server: {
     open: '/dist/counter.js',
-    port: 80, // sudo lsof -i tcp:80 查看占用, kill -9 PID 强制终止
+    port: 80, // sudo lsof -i tcp:80 查看占用 PID, kill -9 PID 强制终止
     strictPort: true, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口。
   }
 })
